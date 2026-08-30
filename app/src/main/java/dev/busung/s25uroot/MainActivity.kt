1393fd97d55ef6948c241ff49c78b951187599b5
Daniel <danird10@proton.me>
2026-08-29 20:13:35 -0500

    app: target KernelSU-Next package
    Signed-off-by: Daniel <danird10@proton.me>

--- app/src/main/java/dev/busung/s25uroot/MainActivity.kt
+++ app/src/main/java/dev/busung/s25uroot/MainActivity.kt
@@ -245,9 +245,9 @@ private val languageOptions = listOf(
 )
 
 private const val KERNEL_SU_MANAGER_URL =
-    "https://github.com/tiann/KernelSU/releases/download/v3.2.5/KernelSU_v3.2.5_32525-release.apk"
-private const val KERNEL_SU_MANAGER_PACKAGE = "me.weishu.kernelsu"
-private const val KERNEL_SU_HOME_URL = "https://kernelsu.org/"
+    "https://github.com/sarabpal-dev/KernelSU-Next/releases/download/v3.3.0-android12-5.10/KernelSU_Next_v3.3.0-release.apk"
+private const val KERNEL_SU_MANAGER_PACKAGE = "com.rifsxd.ksunext"
+private const val KERNEL_SU_HOME_URL = "https://kernelsu-next.github.io/webpage/"
 private const val SHIZUKU_MANAGER_PACKAGE = "moe.shizuku.manager"
 private const val SHIZUKU_MANAGER_URL = "https://github.com/thedjchi/Shizuku/releases/"
 
